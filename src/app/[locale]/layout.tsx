d901faf260b616f8867bd0e9664ea90d6c8890df
Quantum <russewelt@gmail.com>
2025-09-28 09:45:01 -0300

    add: fonts and hero styles

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,16 +6,16 @@ import { routing } from "@/i18n/routing";
 import { Locale } from "next-intl";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Sour_Gummy } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const accentFont = Sour_Gummy({
+  variable: "--font-accent",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const baseFont = Inter({
+  variable: "--font-base",
   subsets: ["latin"],
 });
 
@@ -40,7 +40,7 @@ export default async function RootLayout({
   return (
     <html lang={locale} key={locale} suppressHydrationWarning={true}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative flex flex-col overflow-x-clip h-screen max-w-screen`}
+        className={`${accentFont.variable} ${baseFont.variable} antialiased relative flex flex-col overflow-x-clip h-screen max-w-screen`}
       >
         <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>teste</div>;
+  return (
+    <div className="hero max-w-full bg-amber-800">
+      <h1 className="hero-title">Título do Hero</h1>
+      <h2 className="hero-subtitle">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet omnis
+        ea impedit. Modi inventore neque, maiores blanditiis et mollitia
+        accusantium a ratione omnis quam beatae temporibus reprehenderit. Animi,
+        in tempora.
+      </h2>
+    </div>
+  );
 }
