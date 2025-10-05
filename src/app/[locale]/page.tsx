2ca49d01f1f3c2bc96e9018694862c0de974df4e
Quantum <russewelt@gmail.com>
2025-10-04 22:59:30 -0300

    add: Homepage hero section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,28 @@
+import { useTranslations } from "next-intl";
+
 export default function Home() {
+  const hero = useTranslations("Homepage.Hero");
+
   return (
     <main>
-      <section className="hero max-w-full bg-amber-800">
-        <h1 className="hero-title">Título do Hero</h1>
-        <h2 className="hero-subtitle">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet
-          omnis ea impedit. Modi inventore neque, maiores blanditiis et mollitia
-          accusantium a ratione omnis quam beatae temporibus reprehenderit.
-          Animi, in tempora.
+      <section className="relative hero max-w-full bg-black page-side-padding">
+        <h1 className="hero-title text-[var(--white-base)] ">
+          {hero("Title")}
+        </h1>
+        <h2 className="hero-subtitle text-[var(--white-base)]">
+          {hero("Subtitle")}
         </h2>
+        <video
+          className="absolute top-0 left-0 w-full h-full object-cover opacity-50"
+          src="/videos/dogs_playing.webm"
+          autoPlay
+          muted
+          loop
+          playsInline
+          aria-hidden
+          role="presentation"
+        />
       </section>
-      <section className="bg-black"></section>
     </main>
   );
 }
