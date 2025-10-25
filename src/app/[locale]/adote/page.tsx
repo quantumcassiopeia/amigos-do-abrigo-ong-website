45f3e5d0baa64022be923af0e3adb31876c10e04
Quantum <russewelt@gmail.com>
2025-10-25 17:54:43 -0300

    add: AdotePage section

--- src/app/[locale]/adote/page.tsx
+++ src/app/[locale]/adote/page.tsx
@@ -7,6 +7,35 @@ export default function Adote() {
           Toda história merece um final feliz.
         </p>
       </section>
+
+      <section className="general-section page-side-padding ">
+        <h2 className="title text-[var(--green-base)] ">Visite nosso abrigo</h2>
+        <p className="paragraph">
+          Periodicamente, realizamos visitas ao nosso abrigo para que você possa
+          conhecer os{" "}
+          <span className="accent-font text-[var(--green-base)]">aumigos</span>{" "}
+          que precisam de amor. Visite nossa página no{" "}
+          <a
+            className="accent-font text-[var(--green-base)] underline"
+            href="https://www.instagram.com/abrigojoaorosa/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Instagram
+          </a>{" "}
+          para ficar por dentro das datas de visitação ou agende uma visita
+          atráves do email{" "}
+          <a
+            className="accent-font text-[var(--green-base)] underline"
+            href="mailto:visitas@abrigojoaorosa.org
+"
+          >
+            visitas@abrigojoaorosa.org
+          </a>
+          .
+        </p>
+      </section>
     </main>
   );
 }
