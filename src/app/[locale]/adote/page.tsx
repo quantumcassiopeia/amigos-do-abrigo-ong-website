0575ba96c7eeb6f0fcdba20de51f2277a0d03e70
Quantum <russewelt@gmail.com>
2025-10-25 17:29:30 -0300

    add: AdotePage hero section

--- src/app/[locale]/adote/page.tsx
+++ src/app/[locale]/adote/page.tsx
@@ -1,3 +1,12 @@
 export default function Adote() {
-  return <main>Página adote</main>;
+  return (
+    <main>
+      <section className="hero bg-[var(--yellow-base)] page-side-padding">
+        <h1 className="hero-title text-[var(--white-base)]">Adote</h1>
+        <p className="hero-subtitle text-[var(--white-base)]">
+          Toda história merece um final feliz.
+        </p>
+      </section>
+    </main>
+  );
 }
